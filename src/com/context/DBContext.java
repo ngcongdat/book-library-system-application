4fb5467cc9decce8044c36229809962179b65f08
TinyNguyen <ncd.ccpn@gmail.com>
2019-05-06 07:21:47 +0700

    Code for database context

--- src/com/context/DBContext.java
+++ src/com/context/DBContext.java
@@ -5,10 +5,30 @@
  */
 package com.context;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author tiny
  */
 public class DBContext {
+    
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+     public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    
+    /*Insert your other code right after this comment*/
+   
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "<<server-name>>";
+    private final String dbName = "<<database-name>>";
+    private final String portNumber = "<<port-number>>";
+    private final String userID = "<<user-id>>";
+    private final String password = "<<password>>";
     
 }
